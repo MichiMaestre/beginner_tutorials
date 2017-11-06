85369bd41e5ebf2f58b1a4cbc3fdc61ddeedca7d
michi <a17maestre2009@gmail.com>
2017-11-05 22:11:08 -0500

    Added MIT License to cpp files and repository. Added folder with rqt_console screenshots for Warn and Info.

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,18 +1,40 @@
 
-/**
- *@copyright Copyright (c) 2017 Miguel Maestre Trueba
+/** MIT License
+Copyright (c) 2017 Miguel Maestre Trueba
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ *@copyright Copyright 2017 Miguel Maestre Trueba
  *@file listener.cpp
  *@author Miguel Maestre Trueba
  *@brief ROS listener node that receives messages
  */
 
+#include <string>
+#include <sstream>
 #include "ros/ros.h"
 #include "ros/console.h"
 #include "std_msgs/String.h"
+#include "beginner_tutorials/service.h"
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
+
+int i = 0;
 
 /**
  *@brief Function that reads the message and outputs it
@@ -20,6 +42,8 @@
  */
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_DEBUG_STREAM("Callback accesed " << i << "times");
+  i++;
 }
 
 /**
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,5 +1,22 @@
-/**
- *@copyright Copyright (c) 2017 Miguel Maestre Trueba
+/** MIT License
+Copyright (c) 2017 Miguel Maestre Trueba
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ *@copyright Copyright 2017 Miguel Maestre Trueba
  *@file talker.cpp
  *@author Miguel Maestre Trueba
  *@brief ROS publisher node that sends messages
@@ -88,9 +105,6 @@ int main(int argc, char **argv) {
   // ERROR Logging level check
   if (freq <= 0)
     ROS_ERROR_STREAM("Invalid publisher frequency");
-  // WARN Logging level check
-  if (freq < 2)
-    ROS_WARN_STREAM("Frequency too low. Could cause lag");
 
   // DEBUG Logging level check
   ROS_DEBUG_STREAM("Publisher frequency set up to: " << freq);
@@ -115,6 +129,11 @@ int main(int argc, char **argv) {
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
+
+    // WARN Logging level check
+    if (freq < 2)
+      ROS_WARN_STREAM("Frequency too low. Could cause lag");
+
 
     /**
      * The publish() function is how you send messages. The parameter
